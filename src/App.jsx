e1b7355df720ef49b2745b123aa784bf361e83e1
sergioalba28 <121053053+sergioalba28@users.noreply.github.com>
2024-06-11 01:24:08 +0200

    Calculadora creada

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from "react";
+import "./App.css";
+import Calculator from "./components/Calculator";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
